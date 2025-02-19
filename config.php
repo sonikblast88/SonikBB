8550b0b3320c752b3ca60635c143cc49750c462f
sonikblast88 <sonik.blast@gmail.com>
2025-02-19 22:03:46 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+// Настройки за базата данни
+define('DB_HOST', 'localhost'); // Хост на базата данни
+define('DB_USERNAME', 'username'); // Потребителско име за базата данни
+define('DB_PASSWORD', 'password'); // Парола за базата данни
+define('DB_NAME', 'databasename'); // Име на базата данни
+
+// Път до форума
+$path = './'; // или '/sonikbb/' или друга стойност
+
+// Други настройки
+define('SITE_TITLE', 'SonikBB.EU'); // Заглавие на сайта
+define('SITE_VERSION', '0.0.18 Dev'); // Версия на сайта
+date_default_timezone_set('Europe/Sofia'); // часова зона
